d34804c2a8ad6689ed9e6aeddcece6c6e0981d24
Megan Warnock and Ryan McAlpin <ryan@>
2017-05-23 09:44:40 -0700

    add custom array adapter and custom theme

---
+++ app/src/main/java/com/example/guest/myrestaurant/MyRestaurantsArrayAdapter.java
@@ -0,0 +1,33 @@
+package com.example.guest.myrestaurant;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+/**
+ * Created by Guest on 5/23/17.
+ */
+
+public class MyRestaurantsArrayAdapter extends ArrayAdapter {
+    private Context mContext;
+    private String[] mRestaurants;
+    private String[] mCuisines;
+
+    public MyRestaurantsArrayAdapter(Context mContext, int resource, String[] mRestaurants, String[] mCuisnines) {
+        super(mContext, resource);
+        this.mContext = mContext;
+        this.mRestaurants = mRestaurants;
+        this.mCuisines = mCuisnines;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        String restaurant = mRestaurants[position];
+        String cuisine = mCuisines[position];
+        return String.format("%s \nServes great: %s", restaurant, cuisine);
+    }
+
+    @Override
+    public int getCount() {
+        return mRestaurants.length;
+    }
+}
