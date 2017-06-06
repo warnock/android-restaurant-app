1a0464b955ed545d42ed725bd8aec13e55dc47bf
Blake Womack and Megan Warnock <blake@>
2017-06-06 09:42:40 -0700

    add login and create account activity

---
+++ app/src/main/java/com/example/guest/myrestaurant/ui/CreateAccountActivity.java
@@ -0,0 +1,15 @@
+package com.example.guest.myrestaurant.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.guest.myrestaurant.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
